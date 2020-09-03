fc52447150fa8e6f18f7e6a6e951bfda296a6beb
John Doolittle <jdoo.code@gmail.com>
2020-09-03 15:55:13 -0600

    Created non-working 'watched' button, re-styled MovieList

--- src/components/MovieListItem.jsx
+++ src/components/MovieListItem.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 let MovieListItem = ({movie}) => (
-      <div className='movieListItem'>{movie.title}</div>
+      <div className='movieListItem'>
+          <span className="movieContainer">
+            {movie.title}
+          </span>
+          <span className="buttonContainer">
+            <button className="button">
+            Watched
+            </button>
+          </span>
+      </div>
 );
 
 export default MovieListItem;
